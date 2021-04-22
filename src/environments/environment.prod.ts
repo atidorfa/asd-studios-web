2d69e51a16df317ffa1ab672d026b75f019d370f
xburrito1 <65132417+icburrito@users.noreply.github.com>
2021-04-22 17:03:11 -0300

    Full funcionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCKMv1WqNC0kDbmHKSDQVasK5LwN9szpXw",
+    authDomain: "asd-studios.firebaseapp.com",
+    projectId: "asd-studios",
+    storageBucket: "asd-studios.appspot.com",
+    messagingSenderId: "366432704427",
+    appId: "1:366432704427:web:d5e7f2103f7c2d1bbe24ee",
+    measurementId: "G-CYDQ856W49"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCKMv1WqNC0kDbmHKSDQVasK5LwN9szpXw",
+    authDomain: "asd-studios.firebaseapp.com",
+    projectId: "asd-studios",
+    storageBucket: "asd-studios.appspot.com",
+    messagingSenderId: "366432704427",
+    appId: "1:366432704427:web:d5e7f2103f7c2d1bbe24ee",
+    measurementId: "G-CYDQ856W49"
+  }
 };
 
 /*
