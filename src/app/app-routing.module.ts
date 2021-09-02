f9b77ab6123ba5f9f66a74d8cd1f35404783479e
Gonzalo Rivero Baud <riverobaudg@gmail.com>
2021-09-02 00:29:50 -0300

    home documentation test first implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
-import { NewsComponent } from './components/news/news.component';
-import { GamesComponent } from './components/games/games.component';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { TestComponent } from './components/test/test.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'news', component: NewsComponent},
-  {path: 'games', component: GamesComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'test', component: TestComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,13 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { NewsComponent } from './components/news/news.component';
-import { GamesComponent } from './components/games/games.component';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NewsComponent,
-    GamesComponent,
-    AboutComponent,
-    ContactComponent
+    TestComponent
   ],
   imports: [
     BrowserModule,
